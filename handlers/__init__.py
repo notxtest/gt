eb2f37166e6d46a4413d19e721da427594afa4e5
notxtest <timepassxnot@gmail.com>
2025-11-06 20:13:40 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -8,9 +8,12 @@
 
 from .start import register_handlers
 from .group_commands import register_group_commands
+from .bot_management import register_bot_management
+
 
 def register_all_handlers(app):
     register_handlers(app)
     register_group_commands(app)
+    register_bot_management(app)
     print("✅ Group commands registered!")
   
